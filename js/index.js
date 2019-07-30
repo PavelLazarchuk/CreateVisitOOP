7aacc9cd57ddd42f133ef919f328e7d9236ce1cb
Nelia Havrylchyk <gavrilchiknelia@gmail.com>
2019-07-30 14:46:44 +0300

    add cleaner input

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
+const purpose = document.getElementById('purposeVisit');
+const userName = document.getElementById('userName');
+const date = document.getElementById('date');
+const comment = document.getElementById('comment');
+const pressure = document.getElementById('pressure');
+const indexWeight = document.getElementById('indexWeight');
+const illness = document.getElementById('illness');
+const age = document.getElementById('age');
+const lastDateVisit = document.getElementById('lastDateVisit');
+const sel = document.getElementById('select');
+
 class Visit {
     constructor(purposeVisit, dateVisit, fullName, comment) {
         this._purposeVisit = purposeVisit;
         this._fullName = fullName;
         this._dateVisit = dateVisit;
         this._comment = comment;
-        // const div = document.createElement('div');
-        // div.id = id;
-        // div.innerHTML = (content);
-        // div.classList.add('modal-modal');
-        // this.div = div;
     }
     open() {
         const overlay = document.getElementById('back');
@@ -17,20 +23,57 @@ class Visit {
         mainText.style.display = 'none';
         const modal = document.getElementById('modal');
         modal.style.display = 'flex';
-
-        // document.body.appendChild(this.div);
     }
     close() {
         const overlay = document.getElementById('back');
         overlay.classList.remove('modal-overlay');
-        const modalWindow = document.getElementById('modal');
-        modalWindow.remove();
+        const mainText = document.getElementById('main-text');
+        mainText.style.display = 'block';
+        const modal = document.getElementById('modal');
+        modal.style.display = "none";
+        const inputs = document.getElementsByClassName('input');
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].value = '';
+        }
+
+    }
+    static hiddenInput () {
+        pressure.style.display = 'none';
+        indexWeight.style.display = 'none';
+        illness.style.display = 'none';
+        age.style.display = 'none';
+        age.style.display = 'none';
+        lastDateVisit.style.display = 'none';
+    }
+     checkDoctor() {
+        if(sel.options[sel.selectedIndex].text === 'Кардиолог' ) {
+            pressure.style.display = 'block';
+            indexWeight.style.display = 'block';
+            illness.style.display = 'block';
+            age.style.display = 'block';
+            lastDateVisit.style.display = 'none';
+        }
+        if(sel.options[sel.selectedIndex].text === 'Терапевт' ) {
+            age.style.display = 'block';
+            pressure.style.display = 'none';
+            indexWeight.style.display = 'none';
+            illness.style.display = 'none';
+            lastDateVisit.style.display = 'none';
+        }
+        if(sel.options[sel.selectedIndex].text === 'Дантист' ) {
+            lastDateVisit.style.display = 'block';
+            age.style.display = 'none';
+            pressure.style.display = 'none';
+            indexWeight.style.display = 'none';
+            illness.style.display = 'none';
+        }
     }
     createVisit(){
 
     }
 }
-const modal = new Visit("", "modal");
+Visit.hiddenInput();
+const modal = new Visit(purpose, date, userName, comment);
 
 const button = document.getElementById("createModal");
 button.addEventListener("click", modal.open.bind(modal));
@@ -56,5 +99,7 @@ class Dentist extends Visit {
         this._lastDate = lastDate;
     }
 }
-// ButtonCreate.addEventListener('click', );
-// ButtonAddCard.addEventListener('click', );
+
+// const purpose = document.getElementById('purpose').value
+// let newVisit
+// if(select.value ==='dantist') newVisit = new Dentist(purpose)
