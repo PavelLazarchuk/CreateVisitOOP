0a5b5ca54c8b97267fd5e809eaaa5e6ff5bc5cac
Nelia Havrylchyk <gavrilchiknelia@gmail.com>
2019-07-30 10:53:59 +0300

    add modal

--- js/index.js
+++ js/index.js
@@ -4,9 +4,37 @@ class Visit {
         this._fullName = fullName;
         this._dateVisit = dateVisit;
         this._comment = comment;
+        // const div = document.createElement('div');
+        // div.id = id;
+        // div.innerHTML = (content);
+        // div.classList.add('modal-modal');
+        // this.div = div;
     }
-    
+    open() {
+        const overlay = document.getElementById('back');
+        overlay.classList.add('modal-overlay');
+        const mainText = document.getElementById('main-text');
+        mainText.style.display = 'none';
+        const modal = document.getElementById('modal');
+        modal.style.display = 'flex';
+
+        // document.body.appendChild(this.div);
+    }
+    close() {
+        const overlay = document.getElementById('back');
+        overlay.classList.remove('modal-overlay');
+        const modalWindow = document.getElementById('modal');
+        modalWindow.remove();
+    }
+    createVisit(){
+
+    }
 }
+const modal = new Visit("", "modal");
+
+const button = document.getElementById("createModal");
+button.addEventListener("click", modal.open.bind(modal));
+
 class Сardiologist extends Visit {
     constructor(purposeVisit, normalPressure, massIndex, pastIllnesses, age,  fullName, dateVisit, comment) {
         super(purposeVisit, fullName, dateVisit, comment);
@@ -28,5 +56,5 @@ class Dentist extends Visit {
         this._lastDate = lastDate;
     }
 }
-ButtonCreate.addEventListener('click', );
-ButtonAddCard.addEventListener('click', );
+// ButtonCreate.addEventListener('click', );
+// ButtonAddCard.addEventListener('click', );
